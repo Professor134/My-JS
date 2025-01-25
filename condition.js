11318ff7eef100b9b5be15ccf229490514ee3286
Parth Chavan <parthchavan653@gmail.com>
2025-01-25 18:45:14 +0530

    initial '

---
+++ condition.js
@@ -0,0 +1,29 @@
+//condition
+// let a=9;
+// console.log(a);
+// if(a==2)
+// {
+//     console.log(a);
+// }
+// else
+// {
+//     console.log("no match"); 
+// }
+
+//loop
+// for(let i=0;i<10;i++)
+//     console.log(i);
+// console.log(".");
+
+// let i=1;
+// while(i<=10)
+// {
+//     console.log(i);
+//     i++;
+// }
+
+
+    
+
+
+
---
+++ grade.js
@@ -0,0 +1,22 @@
+function grade(score)
+{
+    if(score>=90)
+    {
+        console.log("A");
+    }    
+    else if(score>=80)
+    {
+        console.log("B");
+    }        
+    else
+    {
+        console.log("C");
+    }   
+}
+
+let s1=87;
+console.log(`grade of s1= ${grade(s1)}`);
+let s2=45;
+console.log(`grade of s2= ${grade(s2)}`);
+let s3=95;
+console.log(`grade of s3= ${grade(s3)}`);
